b7161819eed5b3b125734f64b1ac71cd6bf1280c
Bruno F. <brunoodfonteles@gmail.com>
2026-03-31 18:53:58 -0400

    criação models django

--- apps/ong/admin.py
+++ apps/ong/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import DestaqueInicio, ConteudoSobre, Atividade, MembroEquipe
 
-# Register your models here.
+@admin.register(DestaqueInicio)
+class DestaqueInicioAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'subtitulo')
+
+@admin.register(ConteudoSobre)
+class ConteudoSobreAdmin(admin.ModelAdmin):
+    list_display = ('titulo_secao',)
+
+@admin.register(Atividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data')
+    list_filter = ('data',)
+
+@admin.register(MembroEquipe)
+class MembroEquipeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo')
--- apps/ong/models.py
+++ apps/ong/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+# Aba: Início
+class DestaqueInicio(models.Model):
+    titulo = models.CharField("Título do Banner", max_length=100)
+    subtitulo = models.CharField("Subtítulo/Frase", max_length=200, blank=True)
+    imagem = models.ImageField("Imagem de Destaque", upload_to='inicio/')
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name = "Destaque de Início"
+        verbose_name_plural = "Destaques de Início"
+
+# Aba: Sobre
+class ConteudoSobre(models.Model):
+    titulo_secao = models.CharField("Título da Seção", max_length=100)
+    texto_informativo = models.TextField("Texto sobre a ONG")
+    imagem = models.ImageField("Foto Informativa", upload_to='sobre/')
+
+    def __str__(self):
+        return self.titulo_secao
+
+    class Meta:
+        verbose_name = "Conteúdo Sobre"
+        verbose_name_plural = "Conteúdos Sobre"
+
+# Aba: Atividades
+class Atividade(models.Model):
+    nome = models.CharField("Nome da Atividade", max_length=150)
+    data = models.DateField("Data de Realização")
+    descricao = models.TextField("Descrição da Atividade")
+    imagem = models.ImageField("Foto da Atividade", upload_to='atividades/')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Atividade"
+        verbose_name_plural = "Atividades"
+
+# Aba: Equipe (Nova!)
+class MembroEquipe(models.Model):
+    nome = models.CharField("Nome do Integrante", max_length=100)
+    cargo = models.CharField("Cargo/Função", max_length=100)
+    foto = models.ImageField("Foto de Perfil", upload_to='equipe/')
+    linkedin = models.URLField("Link do LinkedIn", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.cargo}"
+
+    class Meta:
+        verbose_name = "Membro da Equipe"
+        verbose_name_plural = "Membros da Equipe"
