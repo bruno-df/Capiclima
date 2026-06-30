3e5379d3680aca8172af953cfea5a576467aadb9
Bruno F. <brunoodfonteles@gmail.com>
2026-06-30 18:18:39 -0400

    feat: register /health/ route in root urls.py for UptimeRobot keep-alive

--- capiclima/urls.py
+++ capiclima/urls.py
@@ -9,9 +9,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from apps.ong.views import health_check
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
+    path('health/', health_check, name='health_check'),
     path('', include('apps.ong.urls')),
 ]
 
